7677225df9d6c115134fc7424ff8a37e73754110
MahmoudAliEid <alim46972@gmail.com>
2024-01-30 02:08:55 +0200

    Add UserViewLeft component to UserViewPage

--- src/views/apps/user/view/UserViewPage.tsx
+++ src/views/apps/user/view/UserViewPage.tsx
@@ -18,6 +18,7 @@ const UserView = ({ tab, invoiceData, userInfo }: Props) => {
   return (
     <Grid container spacing={6}>
       <Grid item xs={12} md={5} lg={4}>
+       {/* @ts-ignore */}
         <UserViewLeft userInfo={userInfo} />
       </Grid>
       <Grid item xs={12} md={7} lg={8}>
