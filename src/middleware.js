8804cdc7fc9070a5ba3841a0201385d79823a529
MahmoudAliEid <alim46972@gmail.com>
2024-02-02 17:00:45 +0200

    Add redirect logic for authentication

--- src/middleware.js
+++ src/middleware.js
@@ -1,34 +1,56 @@
 import { NextResponse } from 'next/server'
 import { verifyAuth } from './jwt'
 
+const getAbsoluteUrl = (req, path) => {
+  // Determine protocol based on the environment
+  const protocol = req.headers['x-forwarded-proto'] || 'http'
+
+  // Determine host based on the environment
+  const host = req.headers['x-forwarded-host'] || req.headers.host || 'localhost:3000'
+
+  return `${protocol}://${host}${path}`
+}
+
 const middleware = async req => {
   const token = req.cookies.get('token')
   const key = req.cookies.get('key')
   const path = req.nextUrl.pathname // Extract the path from the URL
   const validToken = token && key && (await verifyAuth(token, key))
 
-  // Redirect logic
-  // ...
-
-  // If valid token and attempting to access the home page, redirect to a different page
-  if (validToken && path === '/') {
-    return NextResponse.redirect('/dashboards/analytics')
+  // If token is not valid and user is not on the login page, redirect to login
+  if (!validToken && !path.includes('/login') && path.includes('/dashboards/')) {
+    return NextResponse.redirect(getAbsoluteUrl(req, '/login'))
   }
-  if (!validToken && path === '/') {
-    return NextResponse.redirect('/login')
+  if (!validToken && !path.includes('/login')) {
+    return NextResponse.redirect(getAbsoluteUrl(req, '/login'))
   }
 
-  // ** First Time
-  if (!validToken && !path.includes('/login') && path.includes('/dashboards')) {
-    return NextResponse.redirect('/login')
+  // If token is valid and user is on the login, register, or loginFirstTime page, redirect to dashboards/analytics
+  if (validToken && (path === '/login' || path === '/register' || path === '/loginFirstTime')) {
+    return NextResponse.redirect(getAbsoluteUrl(req, '/dashboards/analytics'))
   }
 
-  if (validToken && (path.includes('/login') || path.includes('/register') || path.includes('/loginFirstTime'))) {
-    return NextResponse.redirect('/dashboards/analytics')
+  // If token is valid and user is on the home page, redirect to dashboards/analytics
+  if (validToken && path === '/' && !path.includes('/dashboards')) {
+    return NextResponse.redirect(getAbsoluteUrl(req, '/dashboards/analytics'))
+  }
+
+  // if token is not valid and user is on the dashboards page, redirect to login
+  if (!validToken && !path.includes('/login') && path.includes('/dashboards/')) {
+    return NextResponse.redirect(getAbsoluteUrl(req, '/login'))
+  }
+
+  // If token is not valid and user is on the app page, redirect to login
+  if (!validToken && !path.includes('/login') && path.includes('/app/')) {
+    return NextResponse.redirect(getAbsoluteUrl(req, '/login'))
+  }
+
+  // If token is not valid and user is on the home page, redirect to login
+  if (!validToken && !path.includes('/login') && path === '/') {
+    return NextResponse.redirect(getAbsoluteUrl(req, '/login'))
   }
 
   return NextResponse.next()
-  // ...
 }
 
 export default middleware
