0b6eb9ec90b040a6d80d6c6646ce9dc60a8b93e9
MahmoudAliEid <alim46972@gmail.com>
2024-02-02 15:10:43 +0200

    Refactor middleware.js to include Vercel deployment URL

--- src/middleware.js
+++ src/middleware.js
@@ -7,64 +7,21 @@ export default middleware = async req => {
   const url = req.url
   const validToken = token && key && (await verifyAuth(token, key))
 
-  // console.log(`validToken: ${validToken}`)
+  // Define Vercel deployment URL
+  const vercelDeploymentURL = process.env.VERCEL_URL || 'http://localhost:3000'
 
-  // // ** secretKey
-  // if (secretKey == undefined || token.length === 0 || secretKey == null) {
-  //   if (url.includes('/dashboards') || url == 'http://localhost:3000/') {
-  //     return NextResponse.redirect('http://localhost:3000/login')
-  //   }
-
-  //   return NextResponse.redirect('http://localhost:3000/login')
-  // }
-
-  // // ** token
-  // if (token == undefined || token.length === 0 || token == null) {
-  //   if (url.includes('/dashboards') || url == 'http://localhost:3000/') {
-  //     return NextResponse.redirect('http://localhost:3000/login')
-  //   }
-
-  //   return NextResponse.redirect('http://localhost:3000/login')
-  // }
-
-  // // ** validation
-  // if (validToken) {
-  //   if (url.includes('/login') || url.includes('/register') || url.includes('/loginFirstTime')) {
-  //     return NextResponse.redirect('http://localhost:3000/dashboards/analytics')
-  //   }
-
-  //   return NextResponse.redirect('http://localhost:3000/dashboards/analytics')
-  // }
-
-  //  || req.nextUrl.pathname.startsWith('/')
+  // Redirect logic
+  // ...
 
   // ** First Time
-  if (
-    (!validToken && url != 'http://localhost:3000/login' && url.includes('/dashboards')) ||
-    url == 'http://localhost:3000/'
-  ) {
-    return NextResponse.redirect('http://localhost:3000/login')
+  if (!validToken && !url.includes('/login') && url.includes('/dashboards')) {
+    return NextResponse.redirect(`${vercelDeploymentURL}/login`)
   }
-  if (validToken && (url.includes('/login') || url.includes('/register') || url.includes('/loginFirstTime'))) {
-    return NextResponse.redirect('http://localhost:3000/dashboards/analytics')
 
-    // return NextResponse.next()
+  if (validToken && (url.includes('/login') || url.includes('/register') || url.includes('/loginFirstTime'))) {
+    return NextResponse.redirect(`${vercelDeploymentURL}/dashboards/analytics`)
   }
 
   return NextResponse.next()
-
-  // // ** First Second
-  // if (url.includes('/login') && !validToken) {
-  //   return NextResponse.next()
-  // }
-
-  // if (!validToken && !url.includes('/login') && url.includes('/dashboards')) {
-  //   return NextResponse.redirect('/login')
-  // }
-
-  // if (validToken && (url.includes('/login') || url.includes('/register') || url.includes('/loginFirstTime'))) {
-  //   return NextResponse.redirect('/dashboards/analytics')
-  // }
-
-  // return NextResponse.next()
+  // ...
 }
